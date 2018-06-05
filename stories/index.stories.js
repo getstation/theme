aca732e6c11f68c92c364d6d43d08dfdf8f79b97
Mathieu Débit <mathieu.debit@gmail.com>
2018-06-05 11:16:20 +0200

    Add Storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Welcome', module)
+  .add('to Station Theme', () =>
+    <div>
+      <h1>Welcome</h1>
+    </div>
+);
