33e978e6089875e37298948d7f754988eee4d2ac
Mathieu Débit <mathieu.debit@gmail.com>
2018-06-27 14:54:44 +0200

    Add Switcher

---
+++ lib/components/Switcher/stories.tsx
@@ -0,0 +1,31 @@
+import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
+import { boolean, withKnobs } from '@storybook/addon-knobs';
+import { withNotes } from '@storybook/addon-notes';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import Switcher, {TEXT} from './index';
+
+const containerStyle = {
+    width: 200,
+    height: 50,
+    padding: '5px 0',
+    backgroundColor: '#0E3255',
+};
+
+const story = storiesOf('Components/Switcher', module);
+
+story
+  .addDecorator(withKnobs)
+  .add('Switcher',
+    withInfo({ text: 'NativeAppDockIcon' })(
+      withNotes('A very simple component')(
+        () =>
+        <div style={containerStyle}>
+          <Switcher
+              checked={boolean('checked', true)}
+              onChange={action('onChange')}
+              text={TEXT.YES_NO}
+          />
+        </div>
+      )));
--- lib/index.ts
+++ lib/index.ts
@@ -6,3 +6,4 @@ export * from './gradient';
 export * from './BrowserXThemeProvider';
 export * from './components/Button';
 export * from './components/Icon';
+export * from './components/Switcher';
