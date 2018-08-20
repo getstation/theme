213d10d61b9f6b1882a289efb231cafef855047a
Mathieu Débit <mathieu.debit@gmail.com>
2018-08-20 16:33:52 +0200

    Add Story for ModalWrapper

---
+++ lib/components/ModalWrapper/stories.tsx
@@ -0,0 +1,19 @@
+import { boolean, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { ModalWrapper } from "./index";
+
+const containerStyle = {
+  padding: 40,
+  backgroundColor: 'white',
+  borderRadius: 8,
+  border: '1px solid',
+};
+
+storiesOf('Molecules|Modal Wrapper', module)
+  .addDecorator(withKnobs)
+  .add('Modal Wrapper', () => (
+    <ModalWrapper backgroundOverlay={boolean('backgroundOverlay', true)}>
+      <div style={containerStyle}>Modal Wrapper</div>
+    </ModalWrapper>
+  ));
