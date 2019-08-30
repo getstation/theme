7dbc5ae1d5c61d83bd994d1e2d88356ee28406a5
Mathieu Débit <mathieu+github@debit.email>
2019-08-30 17:50:42 +0200

    wip finished UI

--- lib/components/InputTags/index.tsx
+++ lib/components/InputTags/index.tsx
@@ -21,29 +21,7 @@ function states () {
 
 const theme : any = {
     container: {
-        position: 'relative'
-    },
-    input: {
-        display: 'block',
-        appearance: 'none',
-        border: '1px solid rgba(41, 41, 41, 0.1)',
-        padding: '0px 15px',
-        boxSizing: 'border-box',
-        borderRadius: 30,
-        minWidth: 200,
-        width: '100%',
-        height: 34,
-        lineHeight: '34px',
-        fontSize: 11,
-        fontWeight: 500,
-        transition: 'all 250ms ease-out',
-        color: '#292929',
-        backgroundColor: '#FFFFFF',
-    },
-    inputFocused: {
-        outline: 'none'
-    },
-    inputOpen: {
+        display: 'inline-flex',
     },
     suggestionsContainer: {
         display: 'none'
@@ -51,9 +29,9 @@ const theme : any = {
     suggestionsContainerOpen: {
         display: 'block',
         position: 'absolute',
-        top: 37,
-        minWidth: 200,
-        width: '100%',
+        top: 44,
+        left: 4,
+        width: '97%',
         border: '1px solid #c9c9c9',
         color: '#292929',
         backgroundColor: '#FFFFFF',
@@ -85,28 +63,74 @@ const theme : any = {
 };
 
 const styles = {
+    '@global': {
+        '.react-tagsinput': {
+            display: 'block',
+            appearance: 'none',
+            border: '1px solid rgba(41, 41, 41, 0.1)',
+            padding: 4,
+            boxSizing: 'border-box',
+            borderRadius: 20,
+            width: 300,
+            minHeight: 34,
+            lineHeight: '34px',
+            fontSize: 11,
+            fontWeight: 500,
+            transition: 'all 250ms ease-out',
+            color: '#292929',
+            backgroundColor: '#FFFFFF',
+        },
+        '.react-tagsinput span': {
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+        },
+        '.react-tagsinput-tag': {
+            display: 'inline-flex',
+            alignItems: 'center',
+            backgroundColor: '#eaeaea',
+            margin: '0 5px 5px 0',
+            padding: 5,
+            fontFamily: 'Helvetica, sans-serif',
+            fontSize: 12,
+            borderRadius: 30,
+            lineHeight: 0,
+        },
+        '.react-tagsinput-tag:last-of-type': {
+            marginBottom: 0,
+        },
+        '.react-tagsinput-remove': {
+            cursor: 'pointer',
+            fontWeight: 'bold',
+        },
+        '.react-tagsinput-tag a::before': {
+            content: " x",
+        },
+        '.react-tagsinput-input': {
+            background: 'transparent',
+            border: 0,
+            color: '#777',
+            fontFamily: 'sans-serif',
+            fontSize: 13,
+            fontWeight: 400,
+            marginBottom: 6,
+            marginTop: 1,
+            outline: 'none',
+            padding: 5,
+            width: 80,
+        },
+    },
     roundPicture: {
         margin: '0 8px 0 5px',
     },
     suggestionContent: {
     },
     renderTag: {
-        listStyleType: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        padding: '5px 5px 5px 0',
-        fontFamily: 'Helvetica, sans-serif',
-        fontSize: 13,
-        '&:last-of-type': {
-            marginBottom: 10,
-        }
     },
     renderTagRoundPicture: {
-        marginRight: 13,
+        marginRight: 6,
     },
     renderTagContent: {
-        flex: 1,
     },
     renderTagRemove: {
     },
@@ -168,7 +192,7 @@ class InputTagsImpl extends React.Component<Props, State> {
         const inputProps = {
             ...props,
             onChange: handleOnChange,
-            placeholder: 'Search members from your organization',
+            placeholder: this.state.tags.length == 0 && 'Search members from your organization',
         };
 
         return (
@@ -192,22 +216,22 @@ class InputTagsImpl extends React.Component<Props, State> {
     render () {
         function renderTag(props: Props, renderTagsProps: any) {
             const { classes } = props;
-            const { tag, key, disabled, onRemove, getTagDisplayValue, ...other } = renderTagsProps;
+            const { tag, key, disabled, onRemove, className, classNameRemove, getTagDisplayValue, ...other } = renderTagsProps;
             return (
-                <li key={key} {...other} className={classes.renderTag}>
+                <span key={key} {...other} className={className}>
                     <RoundPicture
                         className={classes.renderTagRoundPicture}
                         item={states()[0]}
-                        size={22}
+                        size={18}
                         borderColor="transparent"
                     />
                     <div className={classes.renderTagContent}>{getTagDisplayValue(tag)}</div>
                     {!disabled &&
-                        <a className={classes.renderTagRemove} onClick={(e) => onRemove(key)}>
+                        <a className={classNameRemove} onClick={(e) => onRemove(key)}>
                             <Icon symbolId={IconSymbol.CROSS} color={'gray'} size={20} />
                         </a>
                     }
-                </li>
+                </span>
             )
         }
 
