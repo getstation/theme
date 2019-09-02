ada4a94353eff7e13a4968a8a621890a79793115
Mathieu Débit <mathieu+github@debit.email>
2019-09-02 16:59:49 +0200

    :eyes:

--- lib/components/InputTags/stories.tsx
+++ lib/components/InputTags/stories.tsx
@@ -16,12 +16,12 @@ storiesOf('Molecules|InputTags', module)
                 () => (
                     <InputTags
                         items={[
-                            { name: 'Guillaume Arm', selected: true, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15768/thumb_avatar_1558702997.png' },
-                            { name: 'Hugo Mano', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15767/thumb_avatar_1558702986.png' },
+                            { name: 'Guillaume Arm', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15768/thumb_avatar_1558702997.png' },
+                            { name: 'Hugo Mano', selected: true, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15767/thumb_avatar_1558702986.png' },
                             { name: 'Maud Miguet', selected: true, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15764/thumb_avatar_1558702913.png' },
                             { name: 'Alexandre Lachèze', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15763/thumb_avatar_1558702855.png' },
-                            { name: 'Mikaël Atier', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/12182/thumb_avatar_1558702845.png' },
-                            { name: 'Julien Berthomier', selected: true, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15766/thumb_avatar_1558702979.png' },
+                            { name: 'Mikaël Atier', selected: true, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/12182/thumb_avatar_1558702845.png' },
+                            { name: 'Julien Berthomier', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/15766/thumb_avatar_1558702979.png' },
                             { name: 'Joël Charles', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/12181/thumb_avatar_1558702850.png' },
                             { name: 'Mathias D', selected: false, picture: 'https://dgivdslhqe3qo.cloudfront.net/careers/members/17076/thumb_avatar_1567069484.jpg' },
                         ]}
